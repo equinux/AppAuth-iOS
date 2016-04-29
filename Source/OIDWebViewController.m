22a7f7bc580502a86a7da4e4f6b24f782518c6db
Marc Haisenko <haisenko@equinux.com>
2016-04-29 09:57:12 +0200

    Fixed a copyright string.

--- Source/OIDWebViewController.m
+++ Source/OIDWebViewController.m
@@ -1,10 +1,20 @@
-//
-//  OIDWebViewController.m
-//  AppAuth
-//
-//  Created by Marc Haisenko on 28.04.16.
-//  Copyright © 2016 Google Inc. All rights reserved.
-//
+/*! @file OIDWebViewController.m
+    @brief AppAuth iOS SDK
+    @copyright
+        Copyright 2016 equinux AG. All Rights Reserved.
+    @copydetails
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
 
 #import <TargetConditionals.h>
 #if !TARGET_OS_IPHONE
